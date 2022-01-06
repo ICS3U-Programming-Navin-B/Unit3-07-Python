3ded5542f6f6b5447d8d9c6d3af7341b9ba2b841
Ubuntu <ubuntu@ip-172-31-7-242.ec2.internal>
2022-01-06 18:23:47 +0000

    added dating.py

---
+++ dating.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Navin Balekomebole
+# Created on: Jan 6 2022
+# This program uses a compound boolean statement
+
+
+def main():
+    # this function uses a compound boolean statement
+
+    # input
+    Age = int(input("Enter your age: "))
+    print("")
+
+    # process & output
+    if Age > 25 and Age < 40:
+        print("You can date my grand child :)")
+    else:
+        print("You can not date my grand child !!!")
+
+
+if __name__ == "__main__":
+    main()
+    
